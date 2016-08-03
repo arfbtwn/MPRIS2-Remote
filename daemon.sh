7677cb3ae90a0a8a6e6db4af264c722f26fd6b83
Nicholas Little <arealityfarbetween@googlemail.com>
2016-08-03 13:05:56 +0100

    script: Set default libmatthew dir if not already

--- daemon.sh
+++ daemon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 PROJECT_DIR=.
-LIBMATTHEW=/usr/lib64/libmatthew-java
+LIBMATTHEW=${LIBMATTHEW:-/usr/lib64/libmatthew-java}
 
-java -Djava.library.path="$PROJECT_DIR/MprisD/libs:$LIBMATTHEW" -classpath "$PROJECT_DIR/out/production/MprisD:$PROJECT_DIR/out/production/Common:$PROJECT_DIR/MprisD/libs/*:$PROJECT_DIR/libs/*" little.nj.Main
+java -Djava.library.path="$PROJECT_DIR/MprisD/libs:$LIBMATTHEW" -classpath "$PROJECT_DIR/out/production/MprisD:$PROJECT_DIR/out/production/Common:$PROJECT_DIR/MprisD/libs/*:$PROJECT_DIR/libs/*" little.nj.Main $*
